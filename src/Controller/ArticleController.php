c6717e234d23a29406be781192dce76ace57e338
heavsta <francoisleduc@ymail.com>
2021-02-03 12:18:28 +0100

    CRUD done

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\Type\ArticleType;
+use App\Form\Type\CommentFormType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function index(): Response
+    {
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'controller_name' => 'ArticleController',
+            'articles' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/articles/add", name="articles_add")
+     */
+    public function create(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // save the form data in $article
+            $article = $form->getData();
+            $article->setTimestamps(new \DateTime('now'));
+            // save new article in db
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/articles/edit/{id}", name="articles_edit")
+     */
+    public function update(int $id, Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There is no article for this id: '.$id
+            );
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // save the form data in $article
+            $article = $form->getData();
+            $article->setTimestamps(new \DateTime('now'));
+            // save new article in db
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render('articles/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/articles/show/{id}", name="articles_show")
+     */
+    public function show(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        $comment = new Comment;
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        return $this->render('articles/show.html.twig', [
+            'article' => $article,
+            'comment_form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/articles/delete/{id}", name="articles_delete")
+     */
+    public function delete(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There is no article for this id: '.$id
+            );
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+        $this->addFlash('success', 'Your article has been successfully deleted!');
+
+        return $this->redirectToRoute('articles');
+    }
+
+    /**
+     * @Route("/articles/json", name="articles_json")
+     */
+    public function getJson(): JsonResponse
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getTitre();
+            },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $serializer = new Serializer([$normalizer], [$encoder]);
+        $jsonArticle = $serializer->serialize($articles, 'json');
+
+        $response = JsonResponse::fromJsonString($jsonArticle);
+        // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/articles/json/{id}", name="articles_json_id")
+     */
+    public function getJsonById(int $id): JsonResponse
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getTitre();
+            },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $serializer = new Serializer([$normalizer], [$encoder]);
+        $jsonArticle = $serializer->serialize($article, 'json');
+
+        $response = JsonResponse::fromJsonString($jsonArticle);
+        // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/articles/json-add", name="articles_json_add", methods={"POST"})
+     */
+    public function addToJson(Request $request, SerializerInterface $serializer)
+    {
+
+        $data = $request->toArray();
+
+        // serialize $data
+        $data = $serializer->serialize($data, 'json');
+        // deserialize $data
+        $data = $serializer->deserialize($data, Article::class, 'json');
+
+        $data->setTimestamps(new \DateTime());
+        // save new article in db
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($data);
+        $entityManager->flush();
+        return new Response();
+    }
+
+    /**
+     * @Route("/articles/json-edit/{id}", name="articles_json_edit", methods={"PUT"})
+     */
+    public function editJson(int $id, Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There is no article for this id: '.$id
+            );
+        }
+
+        $data = $request->toArray();
+
+        $article->setTitre($data["titre"]);
+        $article->setContenu($data["contenu"]);
+        $article->setPicture($data["picture"]);
+        $article->setTimestamps(new \DateTime());
+        $article->setVisible(true);
+        //save edit in db
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response();
+    }
+
+    /**
+     * @Route("/articles/json-delete/{id}", name="articles_json_delete", methods={"DELETE"})
+     */
+    public function DeleteJson(int $id) {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'There is no article for this id: '.$id
+            );
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        return new Response();
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\CategorieArticle;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $visible;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $timestamps;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=CategorieArticle::class, inversedBy="articles")
+     */
+    private $Categories;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="article")
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->Categories = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): self
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getTimestamps(): ?\DateTimeInterface
+    {
+        return $this->timestamps;
+    }
+
+    public function setTimestamps(\DateTimeInterface $timestamps): self
+    {
+        $this->timestamps = $timestamps;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|CategorieArticle[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->Categories;
+    }
+
+    public function addCategory(CategorieArticle $category): self
+    {
+        if (!$this->Categories->contains($category)) {
+            $this->Categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(CategorieArticle $category): self
+    {
+        $this->Categories->removeElement($category);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->titre;
+    }
+}
